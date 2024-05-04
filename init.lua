088703ca24e1aafef964cbede8fadbedf0467f40
Christian Borja <cpborja32@gmail.com>
2024-05-04 16:08:47 -0700

    feat: install barbar plugin and add keymaps

--- init.lua
+++ init.lua
@@ -196,6 +196,10 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- barbar.nvim commands
+vim.keymap.set('n', '<C-,', ':BufferPrevious<CR>', { desc = 'Move to previous tab' })
+vim.keymap.set('n', '<C-.', ':BufferNext<CR>', { desc = 'Move to next tab' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 

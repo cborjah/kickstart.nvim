13e0a5da996c5af50335ed75a7fab1511bdb5228
Christian Borja <cpborja32@gmail.com>
2024-05-12 23:22:59 -0700

    feat: add vscode snippets

--- init.lua
+++ init.lua
@@ -693,12 +693,12 @@ require('lazy').setup({
           -- `friendly-snippets` contains a variety of premade snippets.
           --    See the README about individual language/framework/plugin snippets:
           --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
+          {
+            'rafamadriz/friendly-snippets',
+            config = function()
+              require('luasnip.loaders.from_vscode').lazy_load { paths = '../../Library/Application Support/Code/User/snippets/custom.code-snippets' }
+            end,
+          },
         },
       },
       'saadparwaiz1/cmp_luasnip',

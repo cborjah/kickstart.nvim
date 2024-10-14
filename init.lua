80c9f8830b258c47a980b7d08e6257f825f9ca98
Christian Borja <cpborja32@gmail.com>
2024-10-14 14:26:39 -0700

    feat: configure kanagawa theme to use its 'dragon' flavor by default

--- init.lua
+++ init.lua
@@ -839,6 +839,11 @@ require('lazy').setup({
   }, ]]
   {
     'rebelot/kanagawa.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    init = function()
+      vim.cmd.colorscheme 'kanagawa-dragon'
+    end,
+
     --[[ config = function()
       require('kanagawa').setup {}
     end, ]]
